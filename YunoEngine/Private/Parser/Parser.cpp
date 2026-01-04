3a138faee2dd8cb9c00102dced124286a2abac4d
Cho-hs0004 <huntmd0004@gmail.com>
2026-01-04 17:58:52 +0900

    일단 메쉬 단위 파싱으로 만듬 나중에 구조 정해지면 수정해야함

--- YunoEngine/Private/Parser/Parser.cpp
+++ YunoEngine/Private/Parser/Parser.cpp
@@ -1,2 +1,129 @@
+#include "pch.h"
+#include "YunoRenderer.h"
+#include "IMesh.h"
+#include "YunoEngine.h"
+
 #include <assimp/Importer.hpp>
 #include <assimp/scene.h>
+#include <assimp/postprocess.h>
+
+
+#include "Parser.h"
+
+bool Parser::LoadFile(const std::string& filename)
+{
+    Assimp::Importer importer;
+
+    const aiScene* scene = importer.ReadFile(
+        filename,
+        aiProcess_Triangulate |
+        aiProcess_ConvertToLeftHanded |
+        aiProcess_GenSmoothNormals |
+        aiProcess_CalcTangentSpace
+    );
+
+    if (!scene || !scene->mRootNode)
+        return false;
+
+    CreateNode(scene->mRootNode, scene);
+
+    return true;
+}
+
+void Parser::CreateNode(aiNode* node, const aiScene* scene)
+{
+    for (size_t i = 0; i < node->mNumMeshes; i++)
+    {
+        aiMesh* aiMesh = scene->mMeshes[node->mMeshes[i]];
+        auto meshH = CreateMesh(aiMesh, scene);
+    }
+
+    for (size_t i = 0; i < node->mNumChildren; i++)
+    {
+        CreateNode(node->mChildren[i], scene); //자식 노드 탐색
+    }
+}
+
+MeshHandle Parser::CreateMesh(aiMesh* aiMesh, const aiScene* scene)
+{
+    std::vector<VERTEX_Pos> vtxPos;
+    std::vector<VERTEX_Nrm> vtxNrm;
+    std::vector<VERTEX_UV> vtxUV;
+    std::vector<VERTEX_T> vtxTan;
+    std::vector<VERTEX_B> vtxBi;
+    std::vector<INDEX> indices;
+
+    for (size_t i = 0; i < aiMesh->mNumVertices; i++)
+    {
+        VERTEX_Pos vPos;
+        vPos.x = aiMesh->mVertices[i].x;
+        vPos.y = aiMesh->mVertices[i].y;
+        vPos.z = aiMesh->mVertices[i].z;
+        vtxPos.push_back(vPos);
+
+        if (aiMesh->HasNormals())
+        {
+            VERTEX_Nrm vNrm;
+            vNrm.nx = aiMesh->mNormals[i].x;
+            vNrm.ny = aiMesh->mNormals[i].y;
+            vNrm.nz = aiMesh->mNormals[i].z;
+            vtxNrm.push_back(vNrm);
+        }
+
+        {
+            VERTEX_UV vUV;
+            if (aiMesh->HasTextureCoords(0)) //0이 제일 기본 uv
+            {
+                vUV.u = aiMesh->mTextureCoords[0][i].x;
+                vUV.v = aiMesh->mTextureCoords[0][i].y;
+            }
+            else
+            {
+                vUV = { 0.0f, 0.0f };
+            }
+            vtxUV.push_back(vUV);
+        }
+        
+        if (aiMesh->HasTangentsAndBitangents())
+        {
+            VERTEX_T vtxT;
+            VERTEX_B vtxB;
+
+            vtxT.tx = aiMesh->mTangents[i].x;
+            vtxT.ty = aiMesh->mTangents[i].y;
+            vtxT.tz = aiMesh->mTangents[i].z;
+
+            vtxB.bx = aiMesh->mBitangents[i].x;
+            vtxB.by = aiMesh->mBitangents[i].y;
+            vtxB.bz = aiMesh->mBitangents[i].z;
+
+            vtxTan.push_back(vtxT);
+            vtxBi.push_back(vtxB);
+        }
+    }
+
+    for (size_t i = 0; i < aiMesh->mNumFaces; i++)
+    {
+        aiFace face = aiMesh->mFaces[i];
+        INDEX idx;
+
+        //파싱 플래그로 무조건 삼각형 face
+        idx.V0 = face.mIndices[0]; 
+        idx.V1 = face.mIndices[1]; 
+        idx.V2 = face.mIndices[2];
+
+        indices.push_back(idx);
+    }
+
+    VertexStreams vs;
+    vs.vtx_count = aiMesh->mNumVertices;
+    vs.pos = vtxPos.data();
+    vs.nrm = vtxNrm.data();
+    vs.uv = vtxUV.data();
+    vs.t = vtxTan.data();
+    vs.b = vtxBi.data();
+
+    const auto& renderer = YunoEngine::GetRenderer();
+
+    return renderer->CreateMesh(vs, indices.data(), aiMesh->mNumFaces);
+}
--- YunoEngine/Private/Parser/Parser.h
+++ YunoEngine/Private/Parser/Parser.h
@@ -1,1 +1,14 @@
 #pragma once
+
+class Parser
+{
+public:
+    Parser() = default;
+    virtual ~Parser() = default;
+
+    bool LoadFile(const std::string& filename);
+
+private:
+    void CreateNode(aiNode* node, const aiScene* scene);
+    MeshHandle CreateMesh(aiMesh* aiMesh, const aiScene* scene);
+};
