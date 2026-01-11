e9a0681206a04c82411fd9fa48f9332baac4e8fc
DanJi-NaRi <127278570+DanJi-NaRi@users.noreply.github.com>
2026-01-11 15:43:09 +0900

    필터 복원
    커밋후 필터 깨진거 복원

--- YunoGame/Game/Object/Quad.h
+++ YunoGame/Game/Object/Quad.h
@@ -2,7 +2,7 @@
 
 #include "Unit.h"
 
-// 각 오브젝트 헤더 파일 최상단에는 Unit 헤더가 있어야됨
+// �� ������Ʈ ��� ���� �ֻ�ܿ��� Unit ����� �־�ߵ�
 class Quad : public Unit
 {
 public:
@@ -13,14 +13,10 @@ public:
     bool Update(float dTime = 0) override;
     bool Submit(float dTime = 0) override;
 private:
-    bool CreateMesh() override;      // 메시 생성 (한 번만)
-    bool CreateMaterial() override;  // 머테리얼 생성 (한 번만)
+    bool CreateMesh() override;      // �޽� ���� (�� ����)
+    bool CreateMaterial() override;  // ���׸��� ���� (�� ����)
 
-    //추가 머테리얼
+    //�߰� ���׸���
     MaterialHandle m_addMaterial = 0;
 };
 
-// 주석도
-// 게임 , 엔진
-// 게임쪽 >> 사용자가 어떻게 쓰는지 
-// 엔진쪽 >> 어느 흐름으로 흘러가는지 
