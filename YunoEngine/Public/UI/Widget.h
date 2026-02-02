d2182da07155087a980c78501e1c4b25f34ee732
DanJi-NaRi <bjh000523@naver.com>
2026-02-02 14:50:33 +0900

    버그 수정

--- YunoEngine/Public/UI/Widget.h
+++ YunoEngine/Public/UI/Widget.h
@@ -229,7 +229,7 @@ public:
     virtual bool  Start(); // Create 다 끝나고 호출. 
     virtual void  CreateChild() {};
 
-    bool          UpdateTransform(float dTime = 0); // 트랜스폼 업데이트. 자식에서 호출하면 꼬이니 주의
+    virtual bool  UpdateTransform(float dTime = 0); // 트랜스폼 업데이트. 자식에서 호출하면 꼬이니 주의
     virtual bool  Update(float dTime = 0);  
     virtual bool  Submit(float dTime = 0);
     bool          LastSubmit(float dTime = 0);      // 이거는 오버라이드 X
