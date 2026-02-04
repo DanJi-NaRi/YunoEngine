e57ac87bcfd0f241d84dbbfaf1c7a41e5ff8bf31
DanJi-NaRi <bjh000523@naver.com>
2026-02-04 21:50:35 +0900

    버그 수정
    버그수정

--- YunoGame/Game/GameApp.cpp
+++ YunoGame/Game/GameApp.cpp
@@ -91,12 +91,11 @@ bool GameApp::OnInit()
    SceneTransitionOptions opt{};
    opt.immediate = true;
 
-   sm->RequestReplaceRoot(std::make_unique<RenderTest>(), opt);  // 본인이 작업중인 씬으로 넣으면 됨
-   //sm->RequestReplaceRoot(std::make_unique<UIScene>(), opt);
+   //sm->RequestReplaceRoot(std::make_unique<RenderTest>(), opt);  // 본인이 작업중인 씬으로 넣으면 됨
+   sm->RequestReplaceRoot(std::make_unique<UIScene>(), opt);
    //sm->RequestReplaceRoot(std::make_unique<WeaponSelectScene>(), opt);
    //sm->RequestReplaceRoot(std::make_unique<Title>(), opt);
    //sm->RequestReplaceRoot(std::make_unique<PhaseScene>(), opt);
-   sm->RequestReplaceRoot(std::make_unique<PhaseScene>(), opt);
 
 
    // UI 재사용 쿼드 제작
@@ -264,14 +263,14 @@ void GameApp::OnUpdate(float dt)
 
 
 
-    if (acc >= 1.0f)
-    {
-        std::cout << "[GameApp] dt = " << dt << "\n";
-        const float fps = static_cast<float>(frameCount) / acc;
-        std::cout << "[GameApp] FPS = " << fps << "\n";
-        acc = 0.0f;
-        frameCount = 0;
-    }
+    //if (acc >= 1.0f)
+    //{
+    //    std::cout << "[GameApp] dt = " << dt << "\n";
+    //    const float fps = static_cast<float>(frameCount) / acc;
+    //    std::cout << "[GameApp] FPS = " << fps << "\n";
+    //    acc = 0.0f;
+    //    frameCount = 0;
+    //}
 
 
 
