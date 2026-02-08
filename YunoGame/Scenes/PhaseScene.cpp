d21dc3f1fc0cb6188925d3e1d83f1bfc1c235701
rlawkdgn123 <rlawkdgn123@naver.com>
2026-02-08 14:38:24 +0900

    PhaseSceneCreate 수정
    배치 정리 및 사이즈 조절

--- YunoGame/Scenes/PhaseScene.cpp
+++ YunoGame/Scenes/PhaseScene.cpp
@@ -42,50 +42,36 @@ bool PhaseScene::OnCreateScene()
     CreateWidget<Card>(L"tstCard", Float2(100, 135), XMFLOAT3(300, 100, 0), XMFLOAT3(0.5f, 0.5f, 0.5f), UIDirection::LeftTop);
 
     
-    {
+    // 테스트용 사이즈
+    /*{
         CreateWidget<CardConfirmPanel>(L"CardConformPanel", Float2(700, 500), XMFLOAT3(0, clientY, 0), XMFLOAT3(1, 1, 1), UIDirection::LeftBottom);
         CreateWidget<Minimap>(L"Minimap", Float2(500, 500), XMFLOAT3(clientX * 0.5f, clientY, 0), XMFLOAT3(1, 1, 1), UIDirection::Bottom);
         CreateWidget<CardSelectionPanel>(L"CardSelectionPanel", Float2(700, 600), XMFLOAT3(clientX, clientY, 0), XMFLOAT3(1, 1, 1), UIDirection::RightBottom);
+    }*/
+
+
+    // 원본 사이즈
+    {
+        CreateWidget<CardConfirmPanel>(L"CardConformPanel", Float2(1566, 578), XMFLOAT3(0, clientY, 0), XMFLOAT3(1, 1, 1), UIDirection::LeftBottom);
+        CreateWidget<Minimap>(L"Minimap", Float2(730, 616), XMFLOAT3(clientX * 0.5f, clientY, 0), XMFLOAT3(1, 1, 1), UIDirection::Bottom);
+        CreateWidget<CardSelectionPanel>(L"CardSelectionPanel", Float2(1513, 578), XMFLOAT3(clientX, clientY, 0), XMFLOAT3(1, 1, 1), UIDirection::RightBottom);
     }
-    //CreateWidget<CardConfirmPanel>(L"CardConformPanel", Float2(1566, 578), XMFLOAT3(0, clientY, 0), XMFLOAT3(1, 1, 1), UIDirection::LeftBottom);
-    
-    //CreateWidget<Minimap>(L"Minimap", Float2(730, 616), XMFLOAT3(clientX*0.5f, clientY, 0), XMFLOAT3(1, 1, 1), UIDirection::Bottom);
+
 
     //테스트용
     //<WidgetGridLine>(L"_DebugWidgetGridLine", Float2(90, 90), XMFLOAT3(clientX*0.5f, clientY * 0.5f, 0), 0, XMFLOAT3(1, 1, 1), UIDirection::Center);
     //CreateWidget<MinimapTile>(L"_tstTile", Float2(90, 90), XMFLOAT3(clientX * 0.5f, clientY * 0.5f, 0), 0, XMFLOAT3(1, 1, 1), UIDirection::Center);
     //std::cout << clientX << " : " << clientY << std::endl;
-    //CreateWidget<CardSelectionPanel>(L"CardSelectionPanel", Float2(1513, 578), XMFLOAT3(clientX, clientY, 0), XMFLOAT3(1, 1, 1),  UIDirection::RightBottom);
-
     
-
-    //CreateWidget<CardTable>(L"tstCardTable", XMFLOAT3(500, 500, 0));
-    //m_uiManager->CreateWidget<CardTable>(L"tstCardTable", XMFLOAT3(500, 500, 0)); 
-
-
-    //auto* slot0 = m_uiManager->CreateWidget<CardSlot>(L"S0", XMFLOAT3(300, 100, 0));
-    //auto* slot1 = m_uiManager->CreateWidget<CardSlot>(L"S1", XMFLOAT3(400, 100, 0));
-    //auto* slot2 = m_uiManager->CreateWidget<CardSlot>(L"S2", XMFLOAT3(500, 100, 0));
-    //auto* slot3 = m_uiManager->CreateWidget<CardSlot>(L"S3", XMFLOAT3(600, 100, 0));
-    //m_uiManager->CreateWidget<Card>(L"tstCard", XMFLOAT3(600, 0, 0));
-    //m_uiManager->CreateWidget<Button>(L"tstBtn", XMFLOAT3(400, 0, 0));
-
-    //m_objectManager->CreateObjectFromFile<Building>(L"Buliding", XMFLOAT3(0, 0, 0), L"../Assets/fbx/Dwarf/Dwarf.fbx");
-    //m_objectManager->CreateObjectFromFile<Building>(L"Buliding", XMFLOAT3(0, 0, 0), L"../Assets/fbx/Building/building.fbx");
-
-
-    //m_objectManager->CreateWidget<Image>(L"tst", XMFLOAT3(0, 0, 0));
-
-
 
     auto* a = CreateWidget<Letterbox>(L"LetterBoxA", Float2(50, 50), XMFLOAT3(0, 0, 0) );
     a->SetRole(Letterbox::Role::BarA);
     auto* b = CreateWidget<Letterbox>(L"LetterBoxB", Float2(50, 50), XMFLOAT3(0, 0, 0));
     b->SetRole(Letterbox::Role::BarB);
-    //m_minimap = std::make_unique<MinimapGridSystem>(m_uiManager.get(), m_input);
-    //m_minimap->CreateObject(1000, 900, 0);
 
-//m_uiManager->AllParentsSetScale(0.5f); // 일괄 사이즈 조정
+    //m_minimap = std::make_unique<MinimapGridSystem>(m_uiManager.get(), m_input);
+
+m_uiManager->AllParentsSetScale(0.5f); // 일괄 사이즈 조정
 
     return true;
 }
